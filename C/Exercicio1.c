33bb72dc608883df58b170a270de1d81b73b91d2
Mateus Henrique <mateushh213@gmail.com>
2022-09-04 20:56:16 -0300

    commit

---
+++ C/Exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nota;
+    printf("Digite sua nota: ");
+    scanf("%d", &nota);
+    printf("Nota: %d", nota);
+
+
+    return 0;
+}
