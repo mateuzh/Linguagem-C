634c7adf7a28fe05dfcb679469b08b2a31ecda93
Mateus Henrique <mateushh213@gmail.com>
2022-09-24 15:33:47 -0300

    commit

--- Aula08/Ex005.c
+++ Aula08/Ex005.c
@@ -3,14 +3,23 @@
 
 int main()
 {
-    int valor, i;
+    int valor, i, fatorial, soma;
     printf("Digite um valor para calcular seu fatorial: ");
     scanf("%d", &valor);
     i = 0;
-    for (i = valor; i <= 0; i--)
+    fatorial = 0;
+    for (i = valor; i != 0; i--)
     {
-        printf(" %d", i);
+        if (i == valor)
+        {
+            fatorial = i * (i-1);
+        }
+        else if (i != 1)
+        {
+            fatorial = fatorial * (i-1);
+        }
     }
-    
+    printf("O fatorial do valor %d", valor);
+    printf(" foi: %d", fatorial);
     return 0;
 }
